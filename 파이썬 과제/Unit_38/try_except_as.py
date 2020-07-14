01650044a042b6fbdb7f7d233c8383ac594a0f7e
InjiChoi <chldlswl1212@gmail.com>
2020-07-14 16:04:02 +0900

    2020.07.14 third week session01

---
+++ 파이썬 과제/Unit_38/try_except_as.py
@@ -0,0 +1,11 @@
+y=[10, 20, 30]
+
+try:
+    index, x = map(int, input('인덱스와 나눌 숫자를 입력하세요: ').split())
+    print(y[index]/x)
+except ZeroDivisionError as e:
+    print('숫자를 0으로 나눌 수 없습니다.', e)
+except IndexError as e :
+    print('잘못된 인덱스입니다.', a)
+except Exception as e:    # 모든 예외의 에러 메시지를 출력할 때는 Exception을 사용
+    print('예외가 발생했습니다.', e)
