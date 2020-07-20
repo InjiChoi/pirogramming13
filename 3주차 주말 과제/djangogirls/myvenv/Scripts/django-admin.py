74f035add2edeab586b797af292c15817c4800fd
InjiChoi <chldlswl1212@gmail.com>
2020-07-20 21:23:23 +0900

    나의 첫 번째 Django 프로젝트!

---
+++ 3주차 주말 과제/djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ingee\dev\pirogramming\3주차 주말 과제\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
